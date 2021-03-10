415309023e32022afa2588cd24a5e258237342ce
Miller Cy Chan <miller.chan@gmail.com>
2021-03-10 10:21:40 +0800

    Add files via upload

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -409,9 +409,16 @@ $(document).on("click", "img.th", function() {
 		document.body.addEventListener("keyup", keyBoardListener);
 	else
 		$("body").on("paste", retrieveImageFromClipboardAsBase64);
+	$("#orig").click(function() {
+		$(this).next().change(function(ev) {
+			$("#btn_upd").prop("disabled", true).text("Please wait...");
+			var id = baseName(this.files[0].name)[0];
+			loadImage(id, this.files[0], ev);
+		}).trigger("click");
+	});
 	$("img.th").css("z-index", "2").on("dragstart", dragStart)
 	$("img.th, #readme").hover(function() {
-		$("#footer").css("z-index", "-1");
+		$("#footer").css("z-index", $("#btn_upd").is(":disabled") ? "1" : "-1");
 	}, function() {
 		$("#footer").css("z-index", "1");
 	});
