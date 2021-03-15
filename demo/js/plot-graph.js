df358e9a47857d7983ef6874dfd47e64a334f1f3
Miller Cy Chan <miller.chan@gmail.com>
2021-03-15 11:45:05 +0800

    Add files via upload

--- demo/js/plot-graph.js
+++ demo/js/plot-graph.js
@@ -1,15 +1,15 @@
 function funGraph(ctx, axes, func, color, thick) {
-	var xx, yy, dx = 10, x0 = axes.x0, y0 = axes.y0, scale = axes.scale;
-	var iMax = Math.round((ctx.canvas.width-x0)/dx);
-	var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
+	var xx, yy, dx = 1, x0 = axes.x0, y0 = axes.y0, scale = axes.scale;
+	var iMax = Math.round((ctx.canvas.width - x0) / dx);
+	var iMin = axes.doNegativeX ? Math.round(-x0 / dx) : 0;
 	ctx.beginPath();
 	ctx.lineWidth = thick;
 	ctx.strokeStyle = color;
 
-	for (var i=iMin; i<=iMax; ++i) {
+	for (var i=iMin; i<=iMax; ++i) {		
 		xx = dx * i;
 		yy = scale * func(xx / scale);
-		if (i==iMin)
+		if (i == iMin)
 			ctx.moveTo(x0 + xx, y0 - yy);
 		else
 			ctx.lineTo(x0 + xx, y0 - yy);
@@ -20,7 +20,8 @@ function funGraph(ctx, axes, func, color, thick) {
 function showAxes(ctx, axes) {
 	var x0 = axes.x0, w = ctx.canvas.width;
 	var y0 = axes.y0, h = ctx.canvas.height;
-	var xmin = axes.doNegativeX ? 0 : x0;
+	var xmin = axes.doNegativeX ? 0 : x0;	
+
 	ctx.beginPath();
 	ctx.strokeStyle = "rgb(128,128,128)"; 
 	ctx.moveTo(xmin, y0);
@@ -28,19 +29,36 @@ function showAxes(ctx, axes) {
 	ctx.moveTo(x0, 0);
 	ctx.lineTo(x0, h);  // Y axis
 	ctx.stroke();
+	
+	// Draw the Y value texts
+    for(var i = 0, j = 200; i < h; i += 50, j-= 50) {
+		if(j == 0)
+			continue;
+		ctx.fillText(j / 10, x0 + 8, i - 18);
+	}
+	
+	// Draw the X value texts	
+	for(var i = 0; i < w; i += 50) {
+		var left = i - 20;
+		if(i == 200)
+			left += 9;
+		ctx.fillText((i - 200) / 10, left, y0 + 9);
+	}
 }
 
 function draw() {
 	var canvas = document.getElementById("canvas");
-	if (null==canvas || !canvas.getContext)
+	if (null == canvas || !canvas.getContext)
 		return;
 
 	axes = {};
 	ctx = canvas.getContext("2d");
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
 	axes.x0 = .5 + .5 * canvas.width;  // x0 pixels from left to x=0
 	axes.y0 = .5 + .5 * canvas.height; // y0 pixels from top to y=0
-	axes.scale = 100;                 // 10 pixels from x=0 to x=1
+	axes.scale = 10;                 // 10 pixels from x=0 to x=1
 	axes.doNegativeX = true;
 
 	showAxes(ctx, axes);
