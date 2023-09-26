0382f20b4858ac8b7ce76381734fd68274fa496b
Miller Cy Chan <miller.chan@gmail.com>
2023-09-26 14:40:27 +0800

    Add files via upload

--- src/gilbertCurve.js
+++ src/gilbertCurve.js
@@ -33,6 +33,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 			g = (pixel >>> 8) & 0xff,
 			b = (pixel >>> 16) & 0xff,
 			a = (pixel >>> 24) & 0xff;
+		this.yDiff = 0;
 		this.p = [r, g, b, a];
 	}
 	
@@ -43,7 +44,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 	var weights = [];
 	var lookup;
 
-	var DITHER_MAX = 9, ditherMax, thresold;
+	var DITHER_MAX = 9, ditherMax, sortedByYDiff, thresold;
 	var BLOCK_SIZE = 343.0;	
 	
 	function ditherPixel(x, y)
@@ -52,13 +53,15 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 		var pixel = pixels[bidx];
 		var error = new ErrorBox(pixel);
 		var maxErr = DITHER_MAX - 1;
+		var i = sortedByYDiff ? weights.length - 1 : 0;
 		for(var c = 0; c < DITHER_MAX; ++c) {
 			var eb = errorq[c];
 			for(var j = 0; j < eb.p.length; ++j) {
-				error.p[j] += eb.p[j] * weights[c];
+				error.p[j] += eb.p[j] * weights[i];
 				if(error.p[j] > maxErr)
 					maxErr = error.p[j];
 			}
+			i += sortedByYDiff ? -1 : 1;
 		}
 
 		var r_pix = Math.clamp(error.p[0], 0, 0xff) | 0;
@@ -101,8 +104,8 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 
 		var denoise = palette.length > 2;
 		var diffuse = TELL_BLUE_NOISE[bidx & 4095] > thresold;
-		var yDiff = diffuse ? 1 : Y_Diff(r0, g0, b0, r_pix, g_pix, b_pix);
-		var illusion = !diffuse && TELL_BLUE_NOISE[((yDiff * 4096) | 0) & 4095] > thresold;
+		error.yDiff = diffuse ? 1 : Y_Diff(r0, g0, b0, r_pix, g_pix, b_pix);
+		var illusion = !diffuse && TELL_BLUE_NOISE[((error.yDiff * 4096) | 0) & 4095] > thresold;
 
 		var errLength = denoise ? error.p.length - 1 : 0;
 		for(var j = 0; j < errLength; ++j) {
@@ -111,7 +114,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 					error.p[j] = Math.fround(Math.tanh(error.p[j] / maxErr * 20)) * (ditherMax - 1);
 				else {
 					if(illusion)
-						error.p[j] = Math.fround(error.p[j] / maxErr * yDiff) * (ditherMax - 1);
+						error.p[j] = Math.fround(error.p[j] / maxErr * error.yDiff) * (ditherMax - 1);
 					else
 						error.p[j] /= Math.fround(1 + Math.sqrt(ditherMax));
 				}
@@ -119,6 +122,8 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 		}
 
 		errorq.push(error);
+		if(sortedByYDiff)
+			errorq.sort((o1, o2) => o2.yDiff - o1.yDiff);
 	}
 
 	function generate2d(x, y, ax, ay, bx, by) {
@@ -191,6 +196,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 		*/
 		errorq = [];
 		var hasAlpha = this.opts.weight < 0;
+		sortedByYDiff = this.opts.saliencies != null && !hasAlpha && this.opts.palette.length >= 128;
 		this.opts.weight = Math.abs(this.opts.weight);
 		DITHER_MAX = this.opts.weight < .01 ? (this.opts.weight > .0025) ? 25 : 16 : 9;
 		var edge = hasAlpha ? 1 : Math.exp(this.opts.weight) - .25;
