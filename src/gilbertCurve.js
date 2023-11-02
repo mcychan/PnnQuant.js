e92d7cd64516d9e4a0d07de7dffe82036811634d
Miller Cy Chan <miller.chan@gmail.com>
2023-11-02 10:05:35 +0800

    Update gilbertCurve.js

--- src/gilbertCurve.js
+++ src/gilbertCurve.js
@@ -231,7 +231,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 			ditherMax = Math.pow(5 + edge, 2);
 		else if(this.opts.palette.length / this.opts.weight < 3200 && this.opts.palette.length > 16 && this.opts.palette.length < 128)
 			ditherMax = Math.pow(5 + edge, 2);
-		margin = Math.pow(log2(palette.length) - 1, 2);
+		margin = Math.pow(Math.log2(this.opts.palette.length) - 1, 2);
 		thresold = DITHER_MAX > 9 ? -112 : -64;
 		weights = [];
 		lookup = new Uint32Array(65536);
