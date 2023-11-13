58310593fc752a89fc29bcdf24d499e51f378598
Miller Cy Chan <miller.chan@gmail.com>
2023-11-13 09:43:29 +0800

    Update index.js

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,3 +1,17 @@
+if(typeof eventBus === 'undefined') {
+	eventBus = {
+		on(event, callback) {
+			document.addEventListener(event, (e) => callback(e.detail));
+		},
+		dispatch(event, data) {
+			document.dispatchEvent(new CustomEvent(event, { detail: data }));
+		},
+		remove(event, callback) {
+			document.removeEventListener(event, callback);
+		},
+	};
+}
+
 class Scene extends preact.Component {
 	constructor(props) {
 		super(props);
