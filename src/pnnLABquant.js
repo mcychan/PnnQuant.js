ae0088b9edafbf5411c31d7fb7bdd48023da509d
Miller Cy Chan <miller.chan@gmail.com>
2024-04-19 17:43:18 +0800

    Add files via upload

--- src/pnnLABquant.js
+++ src/pnnLABquant.js
@@ -451,7 +451,7 @@ Copyright (c) 2018-2023 Miller Cy Chan
 				ratio = Math.min(1.0, 1.0 - weight);
 			else if(proportional > .04)
 				ratio = Math.min(1.0, weight * Math.exp(1.56));
-			else if(proportional > .03)
+			else if(proportional > .033)
 				ratio = Math.min(1.0, proportional + weight * Math.exp(3.66));
 			else
 				ratio = Math.min(1.0, proportional + weight * Math.exp(1.718));
