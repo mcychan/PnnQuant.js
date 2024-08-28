a907ff9f9b35d1d1360047dc0b475d6e73a08539
Miller Cy Chan <miller.chan@gmail.com>
2024-08-28 23:54:45 +0800

    Update gilbertCurve.js

--- src/gilbertCurve.js
+++ src/gilbertCurve.js
@@ -235,7 +235,7 @@ Copyright (c) 2022 - 2023 Miller Cy Chan
 		var hasAlpha = this.opts.weight < 0;
 		this.opts.weight = Math.abs(this.opts.weight);
 		margin = this.opts.weight < .0025 ? 12 : 6;
-		sortedByYDiff = this.opts.palette.length >= 128 && (hasAlpha ? this.opts.weight < .18 : this.opts.weight >= .04);
+		sortedByYDiff = this.opts.palette.length >= 128 && (hasAlpha ? this.opts.weight < .18 : this.opts.weight >= .052);
 		
 		DITHER_MAX = this.opts.weight < .01 ? (this.opts.weight > .0025) ? 25 : 16 : 9;
 		var edge = hasAlpha ? 1 : Math.exp(this.opts.weight) - .25;
