c49493aaafd69e36ebc4add9f59c8b97cc2134db
Miller Cy Chan <miller.chan@gmail.com>
2025-04-17 18:02:12 +0800

    Update gilbertCurve.js

--- src/gilbertCurve.js
+++ src/gilbertCurve.js
@@ -106,7 +106,8 @@ Copyright (c) 2022 - 2025 Miller Cy Chan
 			g1 = (c2 >>> 8) & 0xff,
 			b1 = (c2 >>> 16) & 0xff;
 		if (nMaxColors < 3 || margin > 6) {
-			if (nMaxColors > 4 && (Y_Diff(r0, g0, b0, r1, g1, b1) > (Math.PI * acceptedDiff) || U_Diff(r0, g0, b0, r1, g1, b1) > (margin * acceptedDiff))) {
+			var delta = (weight > .0015 && weight < .0025) ? beta : Math.PI;
+			if (nMaxColors > 4 && (Y_Diff(r0, g0, b0, r1, g1, b1) > (delta * acceptedDiff) || U_Diff(r0, g0, b0, r1, g1, b1) > (margin * acceptedDiff))) {
 				var kappa = saliencies[bidx] < .4 ? beta * .4 * saliencies[bidx] : beta * .4 / saliencies[bidx];
 				var c1 = saliencies[bidx] < .6 ? pixel : (a_pix << 24) | (b_pix << 16) | (g_pix << 8) | r_pix;
 				c2 = new BlueNoise({weightB: kappa}).diffuse(c1, palette[qPixels[bidx]], strength, x, y);
