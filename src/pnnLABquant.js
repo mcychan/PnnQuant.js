584dd526fffc7d72c53f39897c8567ec43c4745d
Miller Cy Chan <miller.chan@gmail.com>
2025-05-21 09:21:31 +0800

    Update pnnLABquant.js

--- src/pnnLABquant.js
+++ src/pnnLABquant.js
@@ -403,10 +403,7 @@ Copyright (c) 2018-2025 Miller Cy Chan
 		var weight = this.opts.weight = Math.min(0.9, nMaxColors * 1.0 / maxbins);
 		if ((nMaxColors < 16 && weight < .0075) || weight < .001 || (weight > .0015 && weight < .0022))
 			quan_rt = 2;
-		if (weight < .04 && PG < 1 && PG >= coeffs[0][1]) {
-			var delta = Math.exp(1.75) * weight;
-			PG -= delta;
-			PB += delta;
+		if (weight < .03 && PG < 1 && PG >= coeffs[0][1]) {
 			if (nMaxColors >= 64)
 				quan_rt = 0;
 		}
